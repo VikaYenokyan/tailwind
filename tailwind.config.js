9a0f094c58f80fc6c20991405a2f9dd78640a9c1
viktoria.e.dterra <v.enokyan@dterra.ru>
2025-01-30 23:14:04 +0300

    upd

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,22 @@ import plugin from 'tailwindcss/plugin'
 export default {
   content: ['./src/**/*.{pug,js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      primary: ['TT Firs Neue'],
+    },
+    colors: {
+      transparent: 'transparent',
+      blue: '#0070B8',
+      black: '#0B1226',
+      grey: {
+        100: '#E4E4E4',
+        200: '#747884',
+      },
+      white: {
+        100: 'rgba(255, 255, 255, 0.10)',
+        200: '#FFF',
+      }
+    },
     extend: {
       transitionTimingFunction: {
         DEFAULT: 'cubic-bezier(0.33, 1, 0.68, 1)'
@@ -10,24 +26,19 @@ export default {
       transitionDuration: {
         DEFAULT: '0.5s'
       },
-      colors: {
-        dark: '#0c0c0c',
-        light: '#f2f2f2'
+      spacing: {
+        '15px': '15px',
+        '18px': '18px',
+        '22px': '22px',
+        '60px': '60px',
+        '68px': '68px',
+        '890px': '890px',
       }
     },
     screens: {
       desktop: { min: '1201px' },
-      devices: { max: '1200px' },
-      tablet: { max: '1200px', min: '769px' },
-      mobile: { max: '768px' },
-      xxl: { max: '1920px' },
-      ll: { max: '1440px' },
-      d: { max: '1201px' },
-      t: { max: '1024px' },
-      lg: { max: '992px' },
-      m: { max: '768px' },
-      s: { max: '576px' },
-      xs: { max: '375px' }
+      tablet: { max: '1200px', min: '768px' },
+      mobile: { max: '767px', min: '375px' },
     }
   },
   plugins: [
@@ -40,11 +51,6 @@ export default {
             display: 'none !important'
           }
         },
-        '.mobile': {
-          [`@media (min-width: ${screens.tablet.min})`]: {
-            display: 'none !important'
-          }
-        },
         '.tablet': {
           [`@media (min-width: ${screens.desktop.min})`]: {
             display: 'none !important'
@@ -53,11 +59,11 @@ export default {
             display: 'none !important'
           }
         },
-        '.devices': {
-          [`@media (min-width: ${screens.desktop.min})`]: {
+        '.mobile': {
+          [`@media (min-width: ${screens.tablet.min})`]: {
             display: 'none !important'
           }
-        }
+        },
       }
 
       addUtilities(newUtilities, ['responsive'])
